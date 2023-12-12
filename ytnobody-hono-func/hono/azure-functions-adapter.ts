3b4ab82579f81862b8346146dc089f1a4fdb8256
ytnobody <ytnobody@gmail.com>
2023-12-12 03:31:11 +0000

    format and independent from @azure/functions

--- ytnobody-hono-func/hono/azure-functions-adapter.ts
+++ ytnobody-hono-func/hono/azure-functions-adapter.ts
@@ -1,59 +1,61 @@
-import {
+import type {
   Context as AzureFunctionsContext,
-  HttpRequest as AzureFunctionsHttpRequest
-} from "@azure/functions";
-import { Hono } from "hono";
+} from './Context'
+import type {
+  HttpRequest as AzureFunctionsHttpRequest,
+} from './http'
+import type { Hono } from 'hono'
 
 export interface AzureFunctionsHTTPEvent {
-  context: AzureFunctionsContext;
-  req: AzureFunctionsHttpRequest;
+  context: AzureFunctionsContext
+  req: AzureFunctionsHttpRequest
 }
 
 export const handle = (app: Hono) => {
   return async (event: AzureFunctionsHTTPEvent): Promise<AzureFunctionsContext> => {
-    const req = createRequest(event);
-    const res = await app.fetch(req);
+    const req = createRequest(event)
+    const res = await app.fetch(req)
 
-    return createResult(event.context, res);
-  };
-};
+    return createResult(event.context, res)
+  }
+}
 
 const createRequest = (event: AzureFunctionsHTTPEvent): Request => {
-  const urlPath = event.req.url;
-  const url = urlPath;
+  const urlPath = event.req.url
+  const url = urlPath ?? ''
 
-  const headersKV = {};
+  const headersKV: { [key: string]: string } = {}
   for (const [k, v] of Object.entries(event.req.headers)) {
-    if (v) headersKV[k] = v;
+    if (v !== undefined) headersKV[k] = v as string
   }
 
-  const headers = new Headers(headersKV);
-  
-  const method = event.req.method;
+  const headers = new Headers(headersKV)
+
+  const method = event.req.method
   const requestInit: RequestInit = {
     headers,
     method,
-  };
-
-  if (event.req.body) {
-    requestInit.body = JSON.stringify(event.req.body);
   }
 
-  return new Request(url, requestInit);
-};
+  if (event.req.body) {
+    requestInit.body = JSON.stringify(event.req.body)
+  }
+
+  return new Request(url, requestInit)
+}
 
 const createResult = async (
   context: AzureFunctionsContext,
   res: Response
 ): Promise<AzureFunctionsContext> => {
-  const contentType = res.headers.get("content-type");
+  const contentType = res.headers.get('content-type')
 
-  res.headers.forEach((value, key) => {
-    context.res.headers[key] = value;
-  });
+  for (const [k, v] of Object.entries(res.headers)) {
+    context.res.headers[k] = v
+  }
 
-  context.res.headers["Content-Type"] = contentType ?? "text/plain";
-  context.res.body = await res.text();
+  context.res.headers['Content-Type'] = contentType ?? 'text/plain'
+  context.res.body = await res.text()
 
-  return context;
-};
+  return context
+}
